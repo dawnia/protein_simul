92f6ece46cbad0fc7adceaad4bb16f24247e2337
BaeoMaltinsky@gmail.com <baeomaltinsky@gmail.com>
2017-12-04 20:08:24 -0800

    various

--- test2.py
+++ test2.py
@@ -1,20 +1,24 @@
 import time
-from itertools import repeat
 import cryo as cr
-import mrcfile
 from pathos.multiprocessing import Pool
 from scipy.stats import ortho_group as og
 import numpy as np
+import mrcfile
+from itertools import repeat
+
 # number of images
-k = 3
+k = 100
 
 # load data
-mol = mrcfile.open("zika_153.mrc").data
+mol = mrcfile.open("amy_small.mrc").data
 
 t0 = time.time()
 # random rotation matrices
 with Pool() as p:
-    orientations = p.map(og.rvs, repeat(3, k))
+    orientations = list(map(og.rvs, repeat(3,k)))
     images = p.map((lambda R: cr.project_fst(mol, R)), orientations)
     b = cr.reconstruct(images, orientations)
-print(time.time()-t0)
+with mrcfile.new('zika_reconstruction.mrc', overwrite=True) as mrc:
+    mrc.set_data(b)
+ 
+print("Completed", k, "in", time.time()-t0)
